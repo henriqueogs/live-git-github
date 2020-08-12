ba72de78fd92f806a4acba2c314a366ce91b7ba7
Henrique <henriqueogs@gmail.com>
2020-08-11 23:51:17 -0300

    Altera retorno da rota

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 
 app.get('/teste', (req, rest) => {
-        return  rest.json({ message: 'Hello Word'})
+        return  rest.json({ message: 'Hello Word 2'})
 });
 
 app.listen(3333);
